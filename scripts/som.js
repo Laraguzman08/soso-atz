62460d88e216724d641083148e8a28ebf1dbebf0
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-05-09 22:03:28 -0300

    add - mudando a música de fundo do members

--- scripts/som.js
+++ scripts/som.js
@@ -2,3 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     const audio = document.getElementById('fundo-audio');
     audio.volume = 0.10; // Ajusta o volume para 10%
 });
+// o id é único por página, em páginas diferentes pode usar mais de uma vez
