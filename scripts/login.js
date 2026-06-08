6a3586b3dcfc7ca2f1fd8b7e63ba5b7055d1915b
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-06-08 19:11:12 -0300

    add - arrumando ajustes e acrescentando dicas

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,15 @@
-console.log('Login script loaded');
+function fecharDicas() {
+    const menssagem = document.getElementById('dicaMenssagem');
+    menssagem.classList.remove('ativa');
+}
 
 function validarLogin() {
+
+    const menssagem = document.getElementById('dicaMenssagem');
+
+    if (menssagem && menssagem.style.display === 'block') {
+        return false; // Impede o login se as dicas estiverem abertas
+    }
 
     let nome = document.getElementById('nome').value;
     let senha = document.getElementById('senha').value;
@@ -28,4 +37,25 @@ function validarLogin() {
     window.location.href = 'transicao.html'; // Redireciona para a página de transição;
     return false;
 
+}
+
+function dicas() {
+    const menssagem = document.getElementById('dicaMenssagem');
+    const texto = document.getElementById('dicaTexto');
+
+    menssagem.classList.toggle('ativa');
+
+    if (menssagem.classList.contains('ativa')) {
+        texto.textContent = 'Click on the buttons to get hints for username and password!';
+    }
+}
+
+function mostrarDica(tipo) {
+    const texto = document.getElementById('dicaTexto');
+
+    if (tipo === 'nome') {
+        texto.textContent = 'The username is a very romantic nickname 💕 It starts with "Esposa" and ends with "do Wooyoung"💍✨';
+    } else if (tipo === 'senha') {
+        texto.textContent = 'The password is a significant date in the format DDMMYYYY!🎂🎀';
+    }
 }
